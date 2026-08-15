b110faf5610006b6d4e6b65a117a44ec5b8924fd
Wolney Oliveira <wolney.js@outlook.com>
2026-08-15 16:55:11 -0300

    feat: setup web application
    Add foundational files and configuration for the web application,
    including React, Vite, TypeScript, Tailwind CSS, and TanStack Router.

---
+++ apps/web/src/routes/index.tsx
@@ -0,0 +1,13 @@
+import { createFileRoute } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/')({
+  component: Index,
+})
+
+function Index() {
+  return (
+    <div className="p-2">
+      <h1>Rabbit Hole</h1>
+    </div>
+  )
+}
